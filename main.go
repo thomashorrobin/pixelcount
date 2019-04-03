350cc9d3cc4b8ee45afee824cf8fbbdeda69986a
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2019-04-03 19:32:21 +0100

    removed commented code

--- main.go
+++ main.go
@@ -37,13 +37,11 @@ func main() {
 func colorize(img image.Image) {
 	b := image.NewRGBA(img.Bounds())
 	draw.Draw(b, b.Bounds(), img, image.ZP, draw.Src)
-	// var m map[color.RGBA]uint32
 
 	m := make(map[color.Color]uint32)
 
 	for x := 0; x < b.Bounds().Dx(); x++ {
 		for y := 0; y < b.Bounds().Dy(); y++ {
-			// log.Println(b.At(x, y).(color.RGBA))
 			xx := b.At(x, y)
 			m[xx] = m[xx] + 1
 		}
@@ -71,5 +69,4 @@ func colorize(img image.Image) {
 func colorHex(c color.Color) string {
 	newColor := colors.FromStdColor(c)
 	return newColor.ToHEX().String()
-	// return c.(color.RGBAColor).ToHex() //"#" + fmt.Sprintf("%x", c.R.(int)*c.G.(int)*c.B.(int)) // + fmt.Sprintf("%x", c.G) + fmt.Sprintf("%x", c.B)
 }
